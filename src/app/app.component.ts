851e11370bbd47a84abf9568002117615b56f689
Sérgio Zimmermann <sergio-zimmermann22@hotmail.com>
2024-02-09 15:22:53 -0300

    remove imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { AngularFireModule } from "@angular/fire/compat";
-import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
