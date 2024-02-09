cd6fa158f4dcd989bbe8c84a21401945473f364c
Sérgio Zimmermann <sergio-zimmermann22@hotmail.com>
2024-02-09 15:21:10 -0300

    Add environment e Firebase configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AngularFireModule } from "@angular/fire/compat";
+import { environment } from '../environments/environment.development';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyARODJj7_Op0OprqU_aYxIhMmlPeag4ci8",
+        authDomain: "treinos-gym.firebaseapp.com",
+        projectId: "treinos-gym",
+        storageBucket: "treinos-gym.appspot.com",
+        messagingSenderId: "1049567299374",
+        appId: "1:1049567299374:web:e62269ceb3c282242d1cbf"
+
+    }
+};
